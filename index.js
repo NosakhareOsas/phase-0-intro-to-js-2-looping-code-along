abf397ac61bf349a625c9732e2ef33042110213c
Nosa Osagiede <osagiedenosakhare@gmail.com>
2022-06-05 20:45:05 +0100

    Test Complete

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+// Code your solutions in this file ["Charlie", "Samip", "Ali"], "birthday"
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names, n){
+    const messages = [];
+    for (let i = 0; i < names.length; i++){
+        messages[i] = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+    }
+    return messages
+}
+
+function countDown(n){
+    let i = 0;
+    let a = n
+    while(i <= n){
+        console.log(a);
+        a--
+        i++
+    }
+
+}
