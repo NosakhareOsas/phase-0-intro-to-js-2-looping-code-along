cf0591d95553ae94f4ea2b82421cd6cc4c827dc5
Nosa Osagiede <osagiedenosakhare@gmail.com>
2022-06-14 10:38:07 +0100

    I removed a redundant parameter

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solutions in this file ["Charlie", "Samip", "Ali"], "birthday"
 const names = ["Guadalupe", "Ollie", "Aki"];
 
-function writeCards(names, n){
+function writeCards(names){
     const messages = [];
     for (let i = 0; i < names.length; i++){
         messages[i] = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
